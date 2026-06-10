2f8b88b34fb14a2f34a2a592316b7a7030809a0a
denvergeeks <jen@denvergeeks.com>
2026-06-09 22:11:45 -0600

    Add route for fetching external data

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 DiscourseProxySafe::Engine.routes.draw do
   get "/fetch" => "proxy#fetch", defaults: { format: :json }
+  get "/fetch_external" => "proxy#fetch_external", defaults: { format: :json }
 end
 
 Discourse::Application.routes.draw do
